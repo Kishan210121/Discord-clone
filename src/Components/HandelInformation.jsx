b48436c2359b3eae27f6ccaaa58bf082274bc9c9
Kishn0109 <raikaran159@gmail.com>
2022-01-21 15:05:15 +0000

    Dounload component added

--- src/Components/HandelInformation.jsx
+++ src/Components/HandelInformation.jsx
@@ -4,6 +4,7 @@ import Information from "./Information";
 import myimg from "../InformationImd1.svg"
 import HomeImg2 from "../Assests/HomeDiscord_Info_Img2.svg"
 import HomeImg3 from "../Assests/HomeInformationImd2.svg"
+import Download from "./Download";
 // /workspace/Discord-clone/src/Assests/HomeInformationImd1.svg
 // /workspace/Discord-clone/src/Assests/HomeDiscord_Info_Img2.svg
 // /workspace/Discord-clone/src/DiscordHeaderLeft.svg
@@ -13,7 +14,7 @@ function HandelInformation() {
       <Information order="order-0" infoimg={myimg} heading="Create an invite-only place where you belong" pera="Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat."/>
       <Information order="lg:order-1" infoimg={HomeImg2} heading="Create an invite-only place where you belong" pera="Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat."/>
       <Information order="order-0" infoimg={HomeImg3} heading="Create an invite-only place where you belong" pera="Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat."/>
-      {/* <Information/> */}
+      <Download/>
   </div>;
 }
 
