45ab35fdc974b402d175c404957377e61f4df444
Kishn0109 <raikaran159@gmail.com>
2022-01-21 13:49:48 +0000

    information section done

---
+++ src/Components/HandelInformation.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Information from "./Information";
+// import info1Image from "../DiscordHeaderLeft.svg"
+import myimg from "../InformationImd1.svg"
+import HomeImg2 from "../Assests/HomeDiscord_Info_Img2.svg"
+import HomeImg3 from "../Assests/HomeInformationImd2.svg"
+// /workspace/Discord-clone/src/Assests/HomeInformationImd1.svg
+// /workspace/Discord-clone/src/Assests/HomeDiscord_Info_Img2.svg
+// /workspace/Discord-clone/src/DiscordHeaderLeft.svg
+function HandelInformation() {
+   
+  return <div>
+      <Information order="order-0" infoimg={myimg} heading="Create an invite-only place where you belong" pera="Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat."/>
+      <Information order="lg:order-1" infoimg={HomeImg2} heading="Create an invite-only place where you belong" pera="Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat."/>
+      <Information order="order-0" infoimg={HomeImg3} heading="Create an invite-only place where you belong" pera="Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat."/>
+      {/* <Information/> */}
+  </div>;
+}
+
+export default HandelInformation;
